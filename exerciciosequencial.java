aa2f16ed1c56ebb0d1bf8a53b2d6db4f106646eb
Diogo Pincetta <diogohe08@gmail.com>
2024-02-02 10:54:38 -0300

    Exercicío sequencial

---
+++ exerciciosequencial.java
@@ -0,0 +1,23 @@
+package exercios_sequenciais;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int resultado = x + y;
+		
+		System.out.println("SOMA =" + resultado);
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
